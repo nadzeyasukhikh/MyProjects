e522edf967da48d8b729d558112a2e915c9291ad
nadzeya <vitserresvit@gmail.com>
2023-08-06 15:39:44 +0300

    new

--- project.Slide/script.js
+++
@@ -1,7 +0,0 @@
-const slides = document.querySelectorAll('.slide')
-slides.forEach((elem) => {
-elem.onclick = () => {
-    document.querySelector('.slide.active') ?.classList.remove('active')
-    elem.classList.add('active')
-}
-})
